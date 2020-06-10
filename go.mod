256aa3b7a1832f3830a49d27918436ffa4776d1b
Yage Hu <yagehu@qq.com>
2020-06-09 22:03:04 -0700

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yagehu/reactor-controller
+
+go 1.14
---
+++ tools/update-codegen.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+echo "$SCRIPT_ROOT"
+echo "$CODEGEN_PKG"
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/yagehu/reactor-controller/pkg/generated github.com/yagehu/reactor-controller/pkg/apis \
+  reactorcontroller:v1alpha1 \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.."
